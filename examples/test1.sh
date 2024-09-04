b98724b571db87b570623cb5c355c249dc9829a3
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-09-03 23:58:37 -0400

    adding 2 sample test case

--- examples/test1.sh
+++ examples/test1.sh
@@ -1,8 +1,8 @@
-API_KEY="..."
-INPUT="Please use this application and provide me a readme.md file"
+API_KEY="....."
+INPUT="Please convert json into csv format"
 
 # Run the Python script with the desired arguments
-python3 ../app/play.py ../app/play.py --api_key "$API_KEY" \
+python3 ../app/play.py ../examples/sample2.json --api_key "$API_KEY" \
 -i "$INPUT" \
 --temperature 0.7 \
 --max_tokens 600 \
