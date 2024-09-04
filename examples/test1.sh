1f731e694d83e0858cac0b2400755df02c6ab34f
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-09-03 23:46:39 -0400

    testing with 2 source file

--- examples/test1.sh
+++ examples/test1.sh
@@ -1,11 +1,10 @@
-API_KEY="fill-in-your-api-key"
+API_KEY="..."
 INPUT="Please use this application and provide me a readme.md file"
 
 # Run the Python script with the desired arguments
-python3 ../app/play.py --api_key "$API_KEY" \
+python3 ../app/play.py ../app/play.py --api_key "$API_KEY" \
 -i "$INPUT" \
 --temperature 0.7 \
 --max_tokens 600 \
 --model gpt-4 \
---output_file output.txt \
---source ../app/play.py
+--output_file output.txt
