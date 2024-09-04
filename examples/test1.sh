65320a4eef5f5b7db6a6e9ae22af6d2a0b7818f8
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-09-03 22:49:49 -0400

    fixing version from the requirement.txt

--- examples/test1.sh
+++ examples/test1.sh
@@ -1,8 +1,8 @@
-API_KEY="Get Your API KEY FROM OpenAi.com"
-INPUT="Please edit your question here"
+API_KEY="change this to your API key"
+INPUT="Derivative of x^2"
 
 # Run the Python script with the desired arguments
-python3 play.py --api_key "$API_KEY" \
+python3 ../app/play.py --api_key "$API_KEY" \
 -i "$INPUT" \
 --temperature 0.7 \
 --max_tokens 50 \
