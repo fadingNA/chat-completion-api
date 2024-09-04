d8d0846fd54f8b494e811bb4c352be6158baa998
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-09-04 00:09:37 -0400

    final touch on play.py

--- examples/test1.sh
+++ examples/test1.sh
@@ -1,10 +1,10 @@
-API_KEY="....."
-INPUT="Please convert json into csv format"
+API_KEY="YOUR_API_KEY"
+INPUT="Please summarize the key points from the following text:"
 
 # Run the Python script with the desired arguments
-python3 ../app/play.py ../examples/sample2.json --api_key "$API_KEY" \
+python3 ../app/play.py ../examples/computer_vision.txt --api_key "$API_KEY" \
 -i "$INPUT" \
 --temperature 0.7 \
---max_tokens 600 \
+--max_tokens 300 \
 --model gpt-4 \
 --output_file output.txt
