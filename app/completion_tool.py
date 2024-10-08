3fdcdf93dc61240671b21eed0abb2a9d6968ef49
fadingNA <nplodthong@myseneca.ca>
2024-10-07 23:24:46 -0400

    moving logic to clean up file with curly brace to minal object

--- app/completion_tool.py
+++ app/completion_tool.py
@@ -2,9 +2,11 @@
 import sys
 import os
 import asyncio
-from utils import setup_logging, write_to_file
-from imports import requests, pytz, LangChainOpenAI, ChatGroq
-from config import TOOL_NAME, VERSION, OPEN_AI_MODELS_URL, ACCEPTED_FILE_EXTENSIONS
+from utils import *  # noqa F403
+from imports import *  # noqa F403
+from config import TOOL_NAME, VERSION, OPEN_AI_MODELS_URL, config
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Setup logger
 logger = setup_logging()
@@ -12,9 +14,13 @@ logger = setup_logging()
 # Set the timezone
 TIME_ZONE = pytz.timezone('America/Toronto')
 
+print("Hello from completion_tool.py")
+print("config: ", config)
+
 class Minal:
     def __init__(self) -> None:
         self.input_text = None
+        self.file_context = None
         self.output_file = None
         self.api_key = None
         self.temperature = 0.5
@@ -25,16 +31,26 @@ class Minal:
         self.token_usage = False
 
     def parse_arguments(self):
-        """Parse the command-line arguments."""
-        self.input_text = self.get_cli_argument(['--input_text', '-i'], default="Default input text")
-        self.output_file = self.get_cli_argument(['--output', '-o'])
-        self.api_key = self.get_cli_argument(['--api_key', '-a'])
-        self.temperature = float(self.get_cli_argument(['--temperature', '-t'], default=0.5))
-        self.max_tokens = int(self.get_cli_argument('--max_tokens', default=100))
-        self.model = self.get_cli_argument(['--model', '-m'], default="gpt-4")
-        self.provider = self.get_cli_argument('--provider', default="OpenAI API")
-        self.streaming = self.get_cli_argument(['--stream', '-s'], default=False)
-        self.token_usage = self.get_cli_argument(['--token_usage'], default=False)
+        """Parse the command-line arguments, with fallback to config file."""
+        if len(sys.argv) == 1:
+            print(self.display_help())
+            sys.exit(0)
+        elif len(sys.argv) > 1:
+            file_path = sys.argv[1]
+            if os.path.exists(file_path):
+                self.file_input = self._cleanup_text_on_file(file_path)
+            else:
+                logger.error(f"File not found: {file_path}")
+                sys.exit(1)
+        self.input_text = self.get_cli_argument(['--input_text', '-i'], config.get('input_text', "Default input text"))
+        self.output_file = self.get_cli_argument(['--output', '-o'], config.get('output_file'))
+        self.api_key = self.get_cli_argument(['--api_key', '-a'], config.get('api_key'))
+        self.temperature = float(self.get_cli_argument(['--temperature', '-t'], config.get('temperature', 0.5)))
+        self.max_tokens = int(self.get_cli_argument('--max_tokens', config.get('max_tokens', 100)))
+        self.model = self.get_cli_argument(['--model', '-m'], config.get('model', "gpt-4"))
+        self.provider = self.get_cli_argument('--provider', config.get('provider', "OpenAI API"))
+        self.streaming = self.get_cli_argument(['--stream', '-s'], config.get('streaming', False))
+        self.token_usage = self.get_cli_argument(['--token_usage'], config.get('token_usage', False))
 
         if '--version' in sys.argv or '-v' in sys.argv:
             print(f"{TOOL_NAME} version: {VERSION}")
@@ -43,10 +59,10 @@ class Minal:
         if '--help' in sys.argv or '-h' in sys.argv or '--howto' in sys.argv:
             print(self.display_help())
             sys.exit(0)
-    
+
     @staticmethod
     def get_cli_argument(flags, default=None):
-        """Helper function to retrieve CLI arguments."""
+        """Helper function to retrieve CLI arguments, falling back to default."""
         if isinstance(flags, list):
             for flag in flags:
                 if flag in sys.argv:
@@ -102,27 +118,111 @@ class Minal:
             logger.error(f"Failed to retrieve models: {e}")
             return None
         
-    async def generate_completion(self):
+    def _cleanup_text_on_file(self, file_path):
+        """
+        Read the file and escape any curly braces in the content.
+        Args:
+        file_path (str): Path to the file to be processed.
+        
+        Returns:
+        str: The cleaned-up content with escaped curly braces or None if the file could not be processed.
+        """
+        if file_path and os.path.exists(file_path):
+            if file_path.endswith('.json') or file_path.endswith('.txt'):
+                context = get_file_content(file_path)
+                # Escape curly braces
+                context = str(context).replace('{', '{{').replace('}', '}}')
+            elif file_path.endswith('.pdf'):
+                docs = load_pdf(file_path)
+                context = format_docs(docs) if docs else None
+            elif file_path.endswith('.docx'):
+                docs = read_file_docx(file_path)
+                context = format_docs(docs) if docs else None
+            else:
+                context = get_file_content(file_path)
+                # Escape curly braces
+                context = str(context).replace('{', '{{').replace('}', '}}')
+            return context
+        else:
+            logger.error(f"File not found: {file_path}")
+            return None
+    
+    def get_prompt_template(self):
+        """Build the prompt template for LangChain."""
+        # Log the inputs for debugging
+        logger.info(f"file_input: {self.file_input}")
+        logger.info(f"input_text: {self.input_text}")
+        try:
+            if not self.file_input or not isinstance(self.file_input, str):
+                logger.error("file_input is invalid or empty.")
+                return None
+
+            if not self.input_text or not isinstance(self.input_text, str):
+                logger.error("input_text is invalid or empty.")
+                return None
+
+            message = [
+                ("system", 
+                f"""
+                You are a professional analyst working with different contexts.
+                Use the provided context: {self.file_input} and respond based on the user's question.
+                """),
+                ("human", f"{self.input_text}")
+            ]
+
+            # Convert to the proper format for ChatPromptTemplate
+            prompt = ChatPromptTemplate.from_messages(message)
+            return prompt
+
+        except Exception as e:
+            logger.error(f"Error building prompt template: {e}")
+            return None
+
+    def generate_completion(self):
         """Generate completion using Langchain API (GROQ , OpenAI API)."""
         try:
             if not self.api_key:
                 raise ValueError("API Key is required to generate completion.")
             logger.info("Generating completion using OpenAI API.")
+            
+            prompt = self.get_prompt_template()
+            if not prompt:
+                logger.error("Failed to generate prompt.")
+                return
+            response = self.get_response()
 
-            response = await self.get_response()
+            if not response:
+                logger.error("Failed to get response from the provider.")
+                return
+            
+            try:
+                chain_llm = prompt | response 
+                
+                if self.streaming:
+                    _response = asyncio.run(self.handle_streaming(chain_llm))
+                else:
+                    _response = chain_llm.invoke({"input_text": self.input_text})
+            except Exception as e:
+                logger.error(f"Error generating completion: {e}")
+                return
 
+            print(response)
             if response:
+                print(_response.content)
                 logger.info("Completion generated successfully.")
                 if self.output_file:
-                    self._save_output(self.output_file, response)
+                    self._save_output(self.output_file, _response)
             else:
                 logger.error("Failed to generate completion.")
         except KeyError as e:
             logger.error(f"Failed to generate completion: {e}")
             logger.error("Please check the input text and try again.")
     
-    async def get_response(self):
+    def get_response(self):
         """Get response from OpenAI API."""
+
+        logger.info(f"Using provider: {self.provider}")
+        logger.info(f"Using model: {self.model}")
 
         if self.provider == "OpenAI API":
             return LangChainOpenAI(
@@ -131,7 +231,7 @@ class Minal:
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
                 model=self.model
-            ).invoke({"input_text": self.input_text})
+            )
         elif self.provider == "GROQ":
             return ChatGroq(
                 base_url=self.get_cli_argument('--base_url'),
@@ -139,7 +239,7 @@ class Minal:
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
                 model=self.model
-            ).invoke({"input_text": self.input_text})
+            )
         
         return None
     
@@ -186,6 +286,6 @@ class Minal:
     def run(self):
         """Main execution function."""
         self.parse_arguments()
-        asyncio.run(self.generate_completion())
+        self.generate_completion()
             
         
